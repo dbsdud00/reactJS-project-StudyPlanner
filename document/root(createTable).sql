5be80bc813120daf3fd165c2df293ba8d0b85c28
dbsdud00 <dbsdudsw@icloud.com>
2023-08-11 08:56:38 +0900

    11

--- document/root(createTable).sql
+++ document/root(createTable).sql
@@ -1,31 +1,34 @@
 CREATE DATABASE plannerDB;
+CREATE DATABASE imagedb;
+use imageDB;
 use plannerDB;
+DROP TABLE tbl_todo;
 DROP TABLE tbl_date;
 CREATE TABLE tbl_todo (
 td_seq	BIGINT	AUTO_INCREMENT	PRIMARY KEY,
 td_dtseq	BIGINT	NOT NULL	,
-td_subject	VARCHAR(10)	NOT NULL	,
-td_content	VARCHAR(30)	NOT NULL	,
+td_subject	VARCHAR(20)	NOT NULL	,
+td_content	VARCHAR(300)	NOT NULL	,
 td_complete	BOOLEAN	NOT NULL	
 ) AUTO_INCREMENT=1;
 
 CREATE TABLE tbl_date (
-td_seq	BIGINT	AUTO_INCREMENT	PRIMARY KEY,
-td_date	VARCHAR(10)	NOT NULL	,
-td_perf	INT	NOT NULL	
+dt_seq	BIGINT	AUTO_INCREMENT	PRIMARY KEY,
+dt_date	VARCHAR(10)	NOT NULL	,
+dt_perf	INT	NOT NULL	
 ) AUTO_INCREMENT=1;
 
 ALTER TABLE tbl_todo -- tbl_files TABLE 변경
 ADD CONSTRAINT F_PLANNER -- F_BBS 이름으로 제약조건 추가
 FOREIGN KEY (td_dtseq) -- 제약조건은 참조 무결성(FK) 이다.
-REFERENCES tbl_date(td_seq); -- 연결은 Table 은 tbl_bbs 이다.
+REFERENCES tbl_date(dt_seq); -- 연결은 Table 은 tbl_bbs 이다.
 
-INSERT INTO tbl_date (td_dtseq,td_perf)
-VALUES ("2023-08-11"	,0);
+INSERT INTO tbl_date (dt_date,dt_perf)
+VALUES ("2023-08-13"	,0);
 
 
 INSERT INTO tbl_todo (td_dtseq,td_subject,td_content,td_complete)
-VALUES (1,"JAVA","ddd",true);
+VALUES (1,"JS","eee",false);
 
 desc tbl_date;
 desc tbl_todo;
@@ -40,6 +43,8 @@ GROUP BY td_subject;
 SELECT * FROM tbl_todo
 WHERE td_subject = "JS";
 
+SELECT * FROM tbl_date;
+SELECT * FROM tbl_todo;
 
 -- 중복제거 날짜
 SELECT DISTINCT(pl_date) FROM tbl_planner;
